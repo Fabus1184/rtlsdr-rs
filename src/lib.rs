74126f863c7f57f93b5aff8e80578218af534219
Fabian Lippold <fabianlippold1184@gmail.com>
2023-12-17 01:29:29 +0100

    Update to rtlsdr 2.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -259,40 +259,19 @@ impl Device {
     }
 
     /// Get actual frequency the device is tuned to in Hz
-    pub fn get_center_freq(&self) -> Result<u64, String> {
-        match unsafe { sys::rtlsdr_get_center_freq64(self.dev) } {
+    pub fn get_center_freq(&self) -> Result<u32, String> {
+        match unsafe { sys::rtlsdr_get_center_freq(self.dev) } {
             0 => Err("Failed to get center frequency".to_string()),
             freq => Ok(freq),
         }
     }
 
     /// Set the frequency the device is tuned to in Hz
-    pub fn set_center_freq(&mut self, freq: u64) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_center_freq64(self.dev, freq))
+    pub fn set_center_freq(&mut self, freq: u32) -> Result<(), String> {
+        rtlsdr_result!(sys::rtlsdr_set_center_freq(self.dev, freq))
             .map_err(|e| format!("Failed to set center frequency: {}", e))?;
 
         Ok(())
-    }
-
-    /// Set harmonic reception - for R820T/2 tuner
-    /// @param harmonic - receive n'th harmonic. 0 = default for disabling this
-    pub fn set_harmonic_rx(&mut self, harmonic: i32) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_harmonic_rx(self.dev, harmonic))
-            .map_err(|e| format!("Failed to set harmonic rx: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Check, if tuner PLL (frequency) is still locked.
-    /// Tuner/PLL might loose lock (at high frequencies),
-    /// e.g. for temperature reasons
-    pub fn is_tuner_pll_locked(&self) -> Result<bool, String> {
-        match unsafe { sys::rtlsdr_is_tuner_PLL_locked(self.dev) } {
-            0 => Ok(true),
-            1 => Ok(false),
-            -2 => Err("Not supported for devices' tuner".to_string()),
-            e => Err(format!("Failed to check if tuner PLL is locked: {}", e)),
-        }
     }
 
     /// Get actual frequency correction value of the device.
@@ -354,65 +333,11 @@ impl Device {
         Ok(())
     }
 
-    /// Get the bandwidth for the device.
-    /// Zero means automatic BW selection.
-    pub fn get_bandwidth(&self) -> Result<u32, String> {
-        let mut applied_bw = 0;
-        rtlsdr_result!(sys::rtlsdr_set_and_get_tuner_bandwidth(
-            self.dev,
-            0,
-            &mut applied_bw,
-            0
-        ))
-        .map_err(|e| format!("Failed to get bandwidth: {}", e))?;
-
-        Ok(applied_bw)
-    }
-
     /// Set the bandwidth for the device.
     /// @param bw bandwidth in Hz. Zero means automatic BW selection.
     pub fn set_tuner_bandwidth(&mut self, bw: u32) -> Result<(), String> {
         rtlsdr_result!(sys::rtlsdr_set_tuner_bandwidth(self.dev, bw,))
             .map_err(|e| format!("Failed to set bandwidth: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Sets the center of the filtered tuner band(width)
-    /// @param center in Hz.
-    /// Zero means, that band center shall be at zero (=default).
-    /// set if_band_center_freq = +samplerate/4 to have the filtered band centered at output's right half.
-    pub fn set_tuner_band_center(&mut self, center: i32) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_tuner_band_center(self.dev, center))
-            .map_err(|e| format!("Failed to set tuner band center: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Set the mixer sideband for the device.
-    pub fn set_tuner_sideband(&mut self, sideband: Sideband) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_tuner_sideband(self.dev, sideband as i32))
-            .map_err(|e| format!("Failed to set tuner mixer sideband: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Set LNA / Mixer / VGA Device Gain for R820T/2 device is configured to.
-    /// @param lna_gain index in 0 .. 15: 0 == min;   see tuner_r82xx.c table r82xx_lna_gain_steps[]
-    /// @param mixer_gain index in 0 .. 15: 0 == min; see tuner_r82xx.c table r82xx_mixer_gain_steps[]
-    /// @param vga_gain index in 0 .. 15: 0 == -12 dB; 15 == 40.5 dB; => 3.5 dB/step;
-    /// vga_gain index 16 activates AGC for VGA controlled from RTL2832
-    /// see tuner_r82xx.c table r82xx_vga_gain_steps[]
-    pub fn set_tuner_gain_ext(
-        &self,
-        lna_gain: i32,
-        mixer_gain: i32,
-        vga_gain: i32,
-    ) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_tuner_gain_ext(
-            self.dev, lna_gain, mixer_gain, vga_gain
-        ))
-        .map_err(|e| format!("Failed to set tuner gain ext: {}", e))?;
 
         Ok(())
     }
@@ -432,23 +357,6 @@ impl Device {
     pub fn set_tuner_gain_mode(&mut self, manual: bool) -> Result<(), String> {
         rtlsdr_result!(sys::rtlsdr_set_tuner_gain_mode(self.dev, manual as i32))
             .map_err(|e| format!("Failed to set tuner gain mode: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Set the agc_variant for automatic gain mode for the device (only R820T/2).
-    /// Automatic gain mode must be enabled for the gain setter function to work.
-    /// @param if_mode
-    /// 0: set automatic VGA, which is controlled from RTL2832
-    /// -2500 .. +2500: set fixed IF gain in tenths of a dB, 115 means 11.5 dB. use -1 or +1 in case you neither want attenuation nor gain.
-    /// this equals the VGA gain for R820T/2 tuner. exact values (R820T/2) are in range -47 .. 408 in tenth of a dB,
-    /// giving -4.7 .. +40.8 dB. these exact values may slightly change with better measurements.
-    /// 10000 .. 10015: IF gain == VGA index from parameter if_mode set if_mode by index: index := VGA_idx +10000
-    /// 10016 .. 10031: same as 10000 .. 10015, but additionally set automatic VGA
-    /// 10011:          for fixed VGA (=default) of -12 dB + 11 * 3.5 dB = 26.5 dB
-    pub fn set_tuner_if_mode(&mut self, agc_mode: i32) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_tuner_if_mode(self.dev, agc_mode))
-            .map_err(|e| format!("Failed to set tuner agc mode: {}", e))?;
 
         Ok(())
     }
@@ -510,26 +418,6 @@ impl Device {
         Ok(())
     }
 
-    /// Set direct sampling mode with threshold
-    /// @param mode static modes 0 .. 2 as in rtlsdr_set_direct_sampling().
-    /// other modes do automatic switching
-    /// @param freq_threshold direct sampling is used below this frequency, else quadrature mode through tuner
-    ///  set 0 for using default setting per tuner - not fully implemented yet!
-    pub fn set_ds_mode(
-        &mut self,
-        mode: DirectSamplingThreshold,
-        freq_threshold: u32,
-    ) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_ds_mode(
-            self.dev,
-            mode as u32,
-            freq_threshold
-        ))
-        .map_err(|e| format!("Failed to set direct sampling mode with threshold: {}", e))?;
-
-        Ok(())
-    }
-
     /// Get state of the offset tuning mode
     pub fn get_offset_tuning(&self) -> Result<bool, String> {
         rtlsdr_result!(sys::rtlsdr_get_offset_tuning(self.dev))
@@ -541,16 +429,6 @@ impl Device {
     pub fn set_offset_tuning(&mut self, enabled: bool) -> Result<(), String> {
         rtlsdr_result!(sys::rtlsdr_set_offset_tuning(self.dev, enabled as i32))
             .map_err(|e| format!("Failed to set offset tuning mode: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Enable or disable frequency dithering for r820t tuners.
-    /// Must be performed before freq_set().
-    /// Fails for other tuners.
-    pub fn set_dithering(&mut self, enabled: bool) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_dithering(self.dev, enabled as i32))
-            .map_err(|e| format!("Failed to set frequency correction mode: {}", e))?;
 
         Ok(())
     }
@@ -632,17 +510,6 @@ impl Device {
         Ok(())
     }
 
-    /// Read from the remote control (RC) infrared (IR) sensor
-    /// @param buf buffer to write IR signal (MSB=pulse/space, 7LSB=duration*20usec), recommended 128-bytes
-    /// @param buf_len size of buf
-    /// @return 0 if no signal, >0 number of bytes written into buf
-    pub fn ir_query(&self, buf: &mut [u8]) -> Result<i32, String> {
-        let n_read = rtlsdr_result!(sys::rtlsdr_ir_query(self.dev, buf.as_mut_ptr(), buf.len(),))
-            .map_err(|e| format!("Failed to query IR: {}", e))?;
-
-        Ok(n_read)
-    }
-
     /// Enable or disable (the bias tee on) GPIO PIN 0 - if not reconfigured.
     /// See rtlsdr_set_opt_string() option 'T'.
     /// This works for rtl-sdr.com v3 dongles, see http://www.rtl-sdr.com/rtl-sdr-blog-v-3-dongles-user-guide/
@@ -673,196 +540,6 @@ impl Device {
         })?;
 
         Ok(())
-    }
-
-    /// Setup a GPIO pin as output.
-    pub fn set_gpio_output(&mut self, gpio: u8) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_gpio_output(self.dev, gpio))
-            .map_err(|e| format!("Failed to set gpio output: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Setup a GPIO pin as input.
-    pub fn set_gpio_input(&mut self, gpio: u8) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_gpio_input(self.dev, gpio))
-            .map_err(|e| format!("Failed to set gpio input: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Read a bit from a GPIO pin.
-    pub fn get_gpio_bit(&self, gpio: u8) -> Result<i32, String> {
-        let mut bit = 0;
-
-        rtlsdr_result!(sys::rtlsdr_get_gpio_bit(self.dev, gpio, &mut bit))
-            .map_err(|e| format!("Failed to read gpio: {}", e))?;
-
-        Ok(bit)
-    }
-
-    /// Write a bit to a GPIO pin.
-    pub fn set_gpio_bit(&mut self, gpio: u8, bit: i32) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_gpio_bit(self.dev, gpio, bit))
-            .map_err(|e| format!("Failed to write gpio: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Read GPIO byte.
-    pub fn get_gpio_byte(&self) -> Result<i32, String> {
-        let mut byte = 0;
-
-        rtlsdr_result!(sys::rtlsdr_get_gpio_byte(self.dev, &mut byte))
-            .map_err(|e| format!("Failed to read gpio byte: {}", e))?;
-
-        Ok(byte)
-    }
-
-    /// Write GPIO byte.
-    pub fn set_gpio_byte(&mut self, byte: i32) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_gpio_byte(self.dev, byte))
-            .map_err(|e| format!("Failed to write gpio byte: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Set GPIO status
-    pub fn set_gpio_status(&mut self, status: i32) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_gpio_status(
-            self.dev,
-            &status as *const i32 as *mut i32
-        ))
-        .map_err(|e| format!("Failed to set gpio status: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Sets multiple options from a string encoded like "bw=300:agc=0:gain=27.3:dagc=0:T=1".
-    /// this is a helper function, that programs don't need to implement every single option at the command line interface.
-    /// Options are seperated by colon ':'.
-    /// There mustn't be extra spaces between option name and '='.
-    /// option 'f' set center frequency as in rtlsdr_set_center_freq()
-    /// option 'bw' sets tuner bandwidth as in rtlsdr_set_tuner_bandwidth() - but value is in kHz.
-    /// option 'agc' sets tuner gain mode as with rtlsdr_set_tuner_gain_mode():
-    ///     '1' means manual gain mode shall be enabled.
-    /// option 'gain' sets tuner gain as with rtlsdr_set_tuner_gain():
-    ///     values in tenth dB.
-    /// option 'dagc' or 'dgc' de/activates digital agc as with rtlsdr_set_agc_mode().
-    ///     1 to enable.
-    ///     0 to disable.
-    /// option 'ds' set direct sampling as with rtlsdr_set_direct_sampling():
-    ///     '0' to deactivate,
-    ///     '1' or 'i' for I-ADC input,
-    ///     '2' or 'q' for Q-ADC input
-    /// option 't' or 'T' for enabling bias tee on GPIO PIN 0 as with rtlsdr_set_bias_tee():
-    ///     '1' for Bias T on.
-    ///     '0' for Bias T off.
-    /// @param opts described option string
-    /// @param verbose print parsed options to stderr
-    pub fn set_opt_string(&mut self, opts: String, verbose: bool) -> Result<(), String> {
-        let cstr = std::ffi::CString::new(opts).expect("Failed to convert opts to CString");
-
-        rtlsdr_result!(sys::rtlsdr_set_opt_string(
-            self.dev,
-            cstr.as_ptr(),
-            verbose as i32
-        ))
-        .map_err(|e| format!("Failed to set opt string: {}", e))?;
-
-        Ok(())
-    }
-
-    /// Get opt help
-    pub fn get_opt_help(long_info: bool) -> String {
-        let ptr = unsafe { sys::rtlsdr_get_opt_help(long_info as i32) };
-        let str = unsafe { std::ffi::CStr::from_ptr(ptr) };
-        str.to_str()
-            .expect("Failed to convert opt help from CStr")
-            .to_owned()
-    }
-
-    /// Get tuner i2c register
-    pub fn get_tuner_i2c_register(&self, data: &mut [u8]) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_get_tuner_i2c_register(
-            self.dev,
-            data.as_mut_ptr(),
-            data.len() as i32
-        ))
-        .map_err(|e| match e {
-            _ => format!("Failed to get tuner i2c register: {}", e),
-        })?;
-
-        Ok(())
-    }
-
-    /// Exposes/permits hacking of Tuner-specific I2C registers:
-    /// set register once
-    /// @param i2c_register  register address
-    /// @param mask          8-bit bitmask, indicating which bits shall be set
-    /// @param data          8-bit data, which shall be set
-    pub fn set_tuner_i2c_register(
-        &mut self,
-        i2c_register: u32,
-        mask: u8,
-        data: u8,
-    ) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_tuner_i2c_register(
-            self.dev,
-            i2c_register,
-            mask as u32,
-            data as u32
-        ))
-        .map_err(|e| match e {
-            -1 => "Device is not initialized".to_string(),
-            _ => format!("Failed to set tuner i2c register: {}", e),
-        })?;
-
-        Ok(())
-    }
-
-    /// Exposes/permits hacking of Tuner-specific I2C registers:
-    /// set and keep register for future
-    /// @param i2c_register  register address
-    /// @param mask          8-bit bitmask, indicating which bits shall be set
-    /// @param data          8-bit data, which shall be set; data in 0 .. 255 sets override; data > 255 clears override
-    pub fn set_tuner_i2c_override(
-        &mut self,
-        i2c_register: u32,
-        mask: u8,
-        data: u8,
-    ) -> Result<(), String> {
-        rtlsdr_result!(sys::rtlsdr_set_tuner_i2c_override(
-            self.dev,
-            i2c_register,
-            mask as u32,
-            data as u32
-        ))
-        .map_err(|e| match e {
-            -1 => "Device is not initialized".to_string(),
-            _ => format!("Failed to set tuner i2c register persistent: {}", e),
-        })?;
-
-        Ok(())
-    }
-
-    /// Request version id string to identify source and date of library
-    pub fn get_version_id() -> &'static str {
-        let ptr = unsafe { sys::rtlsdr_get_ver_id() };
-        let str = unsafe { std::ffi::CStr::from_ptr(ptr) };
-
-        str.to_str()
-            .expect("Failed to convert version id from CStr")
-    }
-
-    /// Request version numbers of library
-    /// @return (major, minor)
-    pub fn get_version() -> (u16, u16) {
-        let ver = unsafe { sys::rtlsdr_get_version() };
-        let major = (ver >> 16) & 0xFFFF;
-        let minor = ver & 0xFFFF;
-
-        (major as u16, minor as u16)
     }
 }
 
